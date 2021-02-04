83ad427f3dc107f3c41f79909a725681f0823b4b
Marina Aleksyuk <marina.aleksuk@yandex.ru>
2021-02-04 23:02:54 +0300

    реализовать добавление карточек

---
+++ config.js
@@ -0,0 +1,57 @@
+export const initialCards = [
+  {
+    name: "Архыз",
+    link:
+      "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+  },
+  {
+    name: "Байкал",
+    link:
+      "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+  },
+  {
+    name: "Иваново",
+    link:
+      "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+  },
+  {
+    name: "Камчатка",
+    link:
+      "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+  },
+  {
+    name: "Холмогорский район",
+    link:
+      "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+  },
+  {
+    name: "Челябинская область",
+    link:
+      "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+  },
+];
+
+export const popupFields = {
+  add: {
+    heading: "Новое место",
+    firstField: {
+      name: "place-name",
+      placeholder: "Название",
+    },
+    secondField: {
+      name: "link",
+      placeholder: "Ссылка на картинку",
+    },
+  },
+  edit: {
+    heading: "Редактировать профиль",
+    firstField: {
+      name: "user-name",
+      placeholder: "Имя",
+    },
+    secondField: {
+      name: "description",
+      placeholder: "О себе",
+    },
+  },
+};
--- scripts/index.js
+++ scripts/index.js
@@ -1,35 +1,102 @@
-// элементы блока user
-let userName = document.querySelector(".user__name");
-let userDescription = document.querySelector(".user__description");
-let editButton = document.querySelector(".user__button_type_edit");
+import { initialCards, popupFields } from "../config.js";
 
-// элементы блока popup
-let popup = document.querySelector(".popup");
-let closeButton = popup.querySelector(".popup__close-button");
+const userName = document.querySelector(".user__name");
+const userDescription = document.querySelector(".user__description");
+const addButton = document.querySelector(".user__button_type_add");
+const editButton = document.querySelector(".user__button_type_edit");
+const cards = document.querySelector(".cards");
+const popup = document.querySelector(".popup");
 
-// элементы блока form
-let formElement = document.querySelector(".form");
-let nameInput = formElement.querySelector(".form__item_el_name");
-let descriptionInput = formElement.querySelector(".form__item_el_description");
+const renderCards = () => {
+  const cardTemplate = document.querySelector("#card").content;
+
+  // получаем массив карточек для отображения в блоке cards
+  const сardsArray = initialCards.map(({ name, link }) => {
+    const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+
+    cardElement.querySelector(".card__image").src = link;
+    cardElement.querySelector(".card__image").alt = name;
+    cardElement.querySelector(".card__title").textContent = name;
+
+    return cardElement;
+  });
+
+  cards.append(...сardsArray);
+};
 
 const togglePopupVisibility = () => popup.classList.toggle("popup_opened");
 
-const showPopup = () => {
+const openPopup = (type) => {
   togglePopupVisibility();
-
-  nameInput.value = userName.textContent;
-  descriptionInput.value = userDescription.textContent;
+  renderPopupContainer(type);
 };
 
-const formSubmitHandler = (evt) => {
+const closePopup = () => {
+  togglePopupVisibility();
+  popup.querySelector(".popup__container").remove();
+};
+
+const addCard = (name, link) => {
+  const cardTemplate = document.querySelector("#card").content;
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+
+  cardElement.querySelector(".card__image").src = link;
+  cardElement.querySelector(".card__image").alt = name;
+  cardElement.querySelector(".card__title").textContent = name;
+
+  cards.prepend(cardElement);
+};
+
+const formSubmitHandler = (evt, type, ...rest) => {
   evt.preventDefault();
 
-  userName.textContent = `${nameInput.value}`;
-  userDescription.textContent = `${descriptionInput.value}`;
+  const [firstValue, secondValue] = rest;
+
+  if (type === "edit") {
+    userName.textContent = firstValue;
+    userDescription.textContent = secondValue;
+  } else {
+    addCard(firstValue, secondValue);
+  }
 
   togglePopupVisibility();
+  popup.querySelector(".popup__container").remove();
 };
 
-editButton.addEventListener("click", showPopup);
-closeButton.addEventListener("click", togglePopupVisibility);
-formElement.addEventListener("submit", formSubmitHandler);
+const renderPopupContainer = (type) => {
+  const { heading, firstField, secondField } = popupFields[type];
+
+  const popupTemplate = document.querySelector("#popup__container").content;
+  const popupContainer = popupTemplate
+    .querySelector(".popup__container")
+    .cloneNode(true);
+
+  const [firstInput, secondInput] = popupContainer.querySelectorAll(
+    ".form__item"
+  );
+
+  firstInput.name = firstField.name;
+  firstInput.placeholder = firstField.placeholder;
+  secondInput.name = secondField.name;
+  secondInput.placeholder = secondField.placeholder;
+  popupContainer.querySelector(".form__heading").textContent = heading;
+
+  if (type === "edit") {
+    firstInput.value = userName.textContent;
+    secondInput.value = userDescription.textContent;
+  }
+
+  popup.append(popupContainer);
+
+  const closeButton = popup.querySelector(".popup__close-button");
+  const formElement = popup.querySelector(".form");
+
+  closeButton.addEventListener("click", closePopup);
+  formElement.addEventListener("submit", (evt) =>
+    formSubmitHandler(evt, type, firstInput.value, secondInput.value)
+  );
+};
+
+renderCards();
+addButton.addEventListener("click", () => openPopup("add"));
+editButton.addEventListener("click", () => openPopup("edit"));
