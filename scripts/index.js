300d56d0abc9ab7e480ae4dfd014435db63d9b2b
Marina Aleksyuk <marina.aleksuk@yandex.ru>
2021-02-06 21:54:27 +0300

    добавить открытие попапа с картинкой

--- scripts/index.js
+++ scripts/index.js
@@ -9,81 +9,14 @@ const userDescription = document.querySelector(".user__description");
 const addButton = document.querySelector(".user__button_type_add");
 const editButton = document.querySelector(".user__button_type_edit");
 
-const addCard = (name, link) => {
-  const cardTemplate = document.querySelector("#card").content;
-  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
-
-  cardElement.querySelector(".card__image").src = link;
-  cardElement.querySelector(".card__image").alt = name;
-  cardElement.querySelector(".card__title").textContent = name;
-
-  const likeButton = cardElement.querySelector(".card__button_type_like");
-  const trashButton = cardElement.querySelector(".card__button_type_trash");
-
-  likeButton.addEventListener("click", () =>
-    likeButton.classList.toggle("card__button_type_like-active")
-  );
-
-  trashButton.addEventListener("click", () =>
-    trashButton.parentElement.remove()
-  );
-
-  return cardElement;
-};
-
-const renderInitialCards = () => {
-  const сardsArray = initialCards.map(({ name, link }) => addCard(name, link));
-
-  cards.append(...сardsArray);
-};
-
 const togglePopupVisibility = () => popup.classList.toggle("popup_opened");
 
 const removePopupContent = () =>
-  popup.querySelector(".popup__close-button").parentElement.remove();
+  popup.querySelector(".close-button").parentElement.remove();
 
 const closePopup = () => {
   togglePopupVisibility();
   removePopupContent();
-};
-
-const renderPopupContent = (type) => {
-  const { heading, firstField, secondField } = formFields[type];
-
-  const popupTemplate = document.querySelector("#popup__container").content;
-  const popupContainer = popupTemplate
-    .querySelector(".popup__container")
-    .cloneNode(true);
-
-  const [firstInput, secondInput] = popupContainer.querySelectorAll(
-    ".form__item"
-  );
-
-  firstInput.name = firstField.name;
-  firstInput.placeholder = firstField.placeholder;
-  secondInput.name = secondField.name;
-  secondInput.placeholder = secondField.placeholder;
-  popupContainer.querySelector(".form__heading").textContent = heading;
-
-  if (type === "edit") {
-    firstInput.value = userName.textContent;
-    secondInput.value = userDescription.textContent;
-  }
-
-  popup.append(popupContainer);
-
-  const closeButton = popup.querySelector(".popup__close-button");
-  const formElement = popup.querySelector(".form");
-
-  closeButton.addEventListener("click", closePopup);
-  formElement.addEventListener("submit", (evt) =>
-    formSubmitHandler(evt, type, firstInput.value, secondInput.value)
-  );
-};
-
-const openPopup = (type) => {
-  togglePopupVisibility();
-  renderPopupContent(type);
 };
 
 const formSubmitHandler = (evt, type, ...inputValues) => {
@@ -101,6 +34,95 @@ const formSubmitHandler = (evt, type, ...inputValues) => {
 
   togglePopupVisibility();
   removePopupContent();
+};
+
+const renderFrom = (type) => {
+  const { heading, firstField, secondField } = formFields[type];
+
+  const formWrapperTemplate = document.querySelector("#form-wrapper").content;
+  const formWrapper = formWrapperTemplate
+    .querySelector(".form-wrapper")
+    .cloneNode(true);
+
+  const [firstInput, secondInput] = formWrapper.querySelectorAll(".form__item");
+
+  firstInput.name = firstField.name;
+  firstInput.placeholder = firstField.placeholder;
+  secondInput.name = secondField.name;
+  secondInput.placeholder = secondField.placeholder;
+  formWrapper.querySelector(".form__heading").textContent = heading;
+
+  if (type === "edit") {
+    firstInput.value = userName.textContent;
+    secondInput.value = userDescription.textContent;
+  }
+
+  popup.append(formWrapper);
+
+  const closeButton = popup.querySelector(".close-button");
+  const formElement = popup.querySelector(".form");
+
+  closeButton.addEventListener("click", closePopup);
+  formElement.addEventListener("submit", (evt) =>
+    formSubmitHandler(evt, type, firstInput.value, secondInput.value)
+  );
+};
+
+const renderFullImage = (evt) => {
+  const imageWrapperTemplate = document.querySelector("#image-full-wrapper")
+    .content;
+  const imageWrapper = imageWrapperTemplate
+    .querySelector(".image-full-wrapper")
+    .cloneNode(true);
+
+  const imageFull = imageWrapper.querySelector(".image-full-container__image");
+  const caption = imageWrapper.querySelector(".image-full-container__caption");
+  imageFull.alt = evt.target.alt;
+  imageFull.src = evt.target.src;
+  caption.textContent = evt.target.alt;
+
+  popup.append(imageWrapper);
+
+  const closeButton = popup.querySelector(".close-button");
+  closeButton.addEventListener("click", closePopup);
+};
+
+const openPopup = (type, evt) => {
+  togglePopupVisibility();
+
+  type === "zoom" ? renderFullImage(evt) : renderFrom(type);
+};
+
+const addCard = (name, link) => {
+  const cardTemplate = document.querySelector("#card").content;
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+
+  cardElement.querySelector(".card__title").textContent = name;
+
+  const cardImage = cardElement.querySelector(".card__image");
+  const likeButton = cardElement.querySelector(".card__button_type_like");
+  const trashButton = cardElement.querySelector(".card__button_type_trash");
+
+  cardImage.src = link;
+  cardImage.alt = name;
+
+  cardImage.addEventListener("click", (evt) => openPopup("zoom", evt));
+
+  likeButton.addEventListener("click", () =>
+    likeButton.classList.toggle("card__button_type_like-active")
+  );
+
+  trashButton.addEventListener("click", () =>
+    trashButton.parentElement.remove()
+  );
+
+  return cardElement;
+};
+
+const renderInitialCards = () => {
+  const сardsArray = initialCards.map(({ name, link }) => addCard(name, link));
+
+  cards.append(...сardsArray);
 };
 
 renderInitialCards();
